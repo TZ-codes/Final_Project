dd3d440a37c9350491ace96da0a85d2c457d4798
Tyler <Faulknert2016@gmail.com>
2022-06-26 21:01:41 -0400

    update products controller class and addItem function

--- javascript/productController.js
+++ javascript/productController.js
@@ -1,10 +1,46 @@
-class productController {
-    constructor (item) {
-        this.item = [];
+// Create a ItemsController class
+class productsController {
+    // Set up the items and currentId property in the contructor
+    constructor(currentId = 0) {
+        this.items = [];
+        this.currentId = currentId;
     }
-};
 
+    // Create the addItem method
+    addItem(name, description,imageUrl) {
+        const item = {
+            // Increment the currentId property
+            id: this.currentId++,
+            name: name,
+            description: description,
+            imageUrl: imageUrl
+        };
 
-const item1 = new productController(this.item)
+        // Push the item to the items property
+        this.items.push(item);
+    }
+}
+
+const item1 = new productsController(this.items)
 
 console.log(item1);
+
+// const equpiment1 = {
+//     id: 1,
+//     name:
+//     description:
+//     img: 
+//     createdAt: 
+// };
+
+// const equipment2 = {
+//     id: 2,
+//     name:
+//     description: 
+//     img:
+//     createdAt: 
+
+
+
+
+

12912f1347b327484896e90b8f5c57f16dcf4533
HEAmmon <104596238+HEAmmon@users.noreply.github.com>
2022-06-27 11:04:33 -0400

    tasks 4-6

---
+++ javascript/productController.js
@@ -0,0 +1,41 @@
+/*class productController {
+    constructor (item) {
+        this.item = [];
+    }
+};
+
+
+const item1 = new productController(this.item)
+
+console.log(item1);*/
+
+
+// Haley's Code
+class ProductController {
+    constructor (currentId = 0) {
+        this.products = [];
+        this.currentId = currentId;
+    }
+    addProduct(name, description, img, createdAt=Date()) {
+        const product = {
+            id: this.currentId++,
+            name: name,
+            description: description,
+            img: img,
+            createdAt: createdAt
+        };
+        this.products.push(product);
+
+        localStorage.setProduct("products", JSON.stringify(this.products));
+    }
+    loadProductsFromLocalStorage() {
+        const storageProducts = localStorage.getProduct("products")
+        if (storageProducts) {
+            const products = JSON.parse(storageProducts)
+            for (let i = 0, size = products.length; i < size; i++) {
+                const products = products[i];
+                this.products.push(product);           
+            }
+        }
+    }
+}
